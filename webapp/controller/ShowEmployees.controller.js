b2f243ebbd40f856e75eab0e2913fb3d1dd1e816
felipebuenotrabajo <felipebuenotrabajo@gmail.com>
2021-08-09 17:11:50 +0000

    Hasta Ascender empleado

--- webapp/controller/ShowEmployees.controller.js
+++ webapp/controller/ShowEmployees.controller.js
@@ -38,11 +38,11 @@ sap.ui.define([
             this.byId("SplitEmployee").to(this.createId("detailEmployee"));
 
             var context = oEvent.getParameter("listItem").getBindingContext("employeeModel");
-            this.newEmployeeId = context.getProperty("EmployeeId");
+            this.EmployeeId = context.getProperty("EmployeeId");
             var detailEmployee = this.byId("detailEmployee");
             //Concatenamos el id de empleado con el mail de usuario
-            detailEmployee.bindElement("employeeModel>/Users(EmployeeId='" + this.newEmployeeId + "',SapId='" + this.getOwnerComponent().SapId + "')");
-            
+            detailEmployee.bindElement("employeeModel>/Users(EmployeeId='" + this.EmployeeId + "',SapId='" + this.getOwnerComponent().SapId + "')");
+
             this._onObjectMached(oEvent);
             // var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             // oRouter.getRoute("RouteShowEmployees").attachPatternMatched(_onObjectMached, this);
@@ -53,29 +53,138 @@ sap.ui.define([
             var sQuery = oEvent.getSource().getValue();
             var filters = [];
             if (sQuery && sQuery.length > 0) {
-				var filter = new Filter("FirstName", FilterOperator.Contains, sQuery);
-				filters.push(filter);
+                var filter = new Filter("FirstName", FilterOperator.Contains, sQuery);
+                filters.push(filter);
             }
-           if (sQuery && sQuery.length > 0) {
-				var filter = new Filter("LastName", FilterOperator.Contains, sQuery);
-				filters.push(filter);
-			}   
-           if (sQuery && sQuery.length > 0) {
-				var filter = new Filter("EmployeeId", FilterOperator.Contains, sQuery);
-				filters.push(filter);
-			}           
-            
-            var oList = this.getView().byId("qwqw");
+            if (sQuery && sQuery.length > 0) {
+                var filter = new Filter("LastName", FilterOperator.Contains, sQuery);
+                filters.push(filter);
+            }
+            if (sQuery && sQuery.length > 0) {
+                var filter = new Filter("EmployeeId", FilterOperator.Contains, sQuery);
+                filters.push(filter);
+            }
+
+            var oList = this.getView().byId("EmployeeList");
             var oBinding = oList.getBinding("items");
             oBinding.filter(filters);
         };
 
         function onGetFireEmployee() {
-
+            sap.m.MessageBox.confirm(this.getView().getModel("i18n").getResourceBundle().getText("showEmployeeConfirmDelete"), {
+                onClose: function (oAction) {
+                    if (oAction === "OK") {
+                        var employeePath = "/Users(EmployeeId='" + this.EmployeeId + "',SapId='" + this.getOwnerComponent().SapId + "')".
+                            this.getView().getModel("employeeModel").remove(employeePath, {
+                                success: function (data) {
+                                    sap.m.MessageToast.show(this.getView().getModel("i18n").getResourceBundle().getText("showEmployeeDeleted"));
+                                    this.byId("SplitEmployee").to(this.createId("detailEmployee"));
+                                }.bind(this),
+                                error: function () {
+                                    sap.m.MessageToast.show(this.getView().getModel("i18n").getResourceBundle().getText("showEmployeeErrorDeleted"));
+                                }.bind(this)
+                            });
+                    }
+                }.bind(this)
+            });
         };
+
         function onPromoteEmployee() {
+            var i18nText = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeTitle");
+            var oPage = new sap.m.Page({
+                title: i18nText,
+            });
+
+            i18nText = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeOK");
+            var oButtonOk = new sap.m.Button("Save", {
+                text: i18nText,
+                tap: [this.Save, this]
+            });
+            i18nText = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeOK");
+            var oButtonCancel = new sap.m.Button("Cancel", {
+                text: "i18nText",
+                tap: [this.Cancel, this]
+            });
+            i18nText = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeOK");
+            var i18nTextSalary = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeSalary");
+            var i18nTextDate = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeDate");
+            var i18nTextComments = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeComments");
+
+            var oDialog = new sap.m.Dialog("Dialog1", {
+                title: "promoteEmployeeTitle",
+                modal: true,
+                contentWidth: "1em",
+                buttons: [oButtonOk, oButtonCancel],
+                content: [
+                    new sap.m.Label({ text: i18nTextSalary }),
+                    new sap.m.Input({
+                        maxLength: 20,
+                        id: "EmployeePromoteSalary"
+                    }),
+                    new sap.m.Label({ text: i18nTextDate }),
+                    new sap.m.Input({
+                        maxLength: 20,
+                        id: "EmployeePromoteDate"
+                    }),
+                    new sap.m.Label({ text: i18nTextComments }),
+                    new sap.m.Input({
+                        maxLength: 3,
+                        id: "EmployeePromoteComments"
+                    }),
+                ]
+            });
+
+            sap.ui.getCore().byId("Dialog1").open();
 
         };
+
+        function Cancel() {
+
+            sap.ui.getCore().byId("Dialog1").close();
+
+        }
+
+        function Save() {
+            i18nTextSalary = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeSalary");
+            i18nTextDate = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeDate");
+            i18nTextComments = this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeComments");
+
+            sap.ui.getCore().byId("Dialog1").close();
+
+            var employeePromoteSalary = sap.ui.getCore().byId(promoteEmployeeSalary).getValue();
+
+            var employeePromoteDate = sap.ui.getCore().byId().getValue(promoteEmployeeDate);
+
+            var employeePromoteComments = sap.ui.getCore().byId(promoteEmployeeComments).getValue();
+
+            this.riseDialog.setModel(new sap.ui.model.json.JSONModel({}), "newRise");
+
+            if (!this.promotedEmployee) {
+                this.promotedEmployee.setModel(new sap.ui.model.json.JSONModel({
+                    Ammount: employeePromoteSalary,
+                    CreationDate: employeePromoteDate,
+                    Comments: employeePromoteComments,
+                }), "newPromoteEmployee");
+                var newRise = this.riseDialog.getModel("newRise");
+                var odata = newRise.getData();
+                var body = {
+                    Ammount: odata.Ammount,
+                    CreationDate: odata.CreationDate,
+                    Comments: odata.Comments,
+                    SapId: this.getOwnerComponent().SapId,
+                    EmployeeId: this.employeeId
+                };
+                this.getView().getModel("employeeModel").create("/Salaries", body, {
+                    success: function () {
+                        sap.m.MessageToast.show(this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeSavedOk"));
+                    }.bind(this),
+                    error: function () {
+                        sap.m.MessageToast.show(this.getView().getModel("i18n").getResourceBundle().getText("promoteEmployeeSavedKO"));
+                    }.bind(this)
+                });
+            }
+
+        }
 
         var Main = Base.extend("EmployeeManager.employeemanager.controller.ShowEmployees", {});
 
@@ -85,6 +194,6 @@ sap.ui.define([
         Main.prototype.onGetFireEmployee = onGetFireEmployee;
         Main.prototype.onPromoteEmployee = onPromoteEmployee;
         Main.prototype._onObjectMached = _onObjectMached;
-        
+
         return Main;
     });
